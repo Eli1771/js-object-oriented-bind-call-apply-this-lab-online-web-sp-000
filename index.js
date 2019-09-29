c633a81d7aa58a20a2cd29f8f4d79eeced0eecb4
elijahmm17 <elijahmm17@gmail.com>
2019-09-29 13:52:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function setThisWithCall(fn, thisValue, arg) {
 function setThisWithApply(fn, thisValue, args) {
   return fn.apply(thisValue, args);
 }
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied(thisValue);
+}
